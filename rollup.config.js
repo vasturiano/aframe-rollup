65a6e3b5d540c978db70776402d8490e1937cb83
Vasco Asturiano <vasturiano@medallia.com>
2017-04-19 14:03:38 -0700

    Exclude use strict, for compat with a-frame es5 code

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
 
 export default {
+    useStrict: false,   // a-frame 0.5 not 'strict' compatible
     format: 'umd',
     plugins: [ nodeResolve() ]
 };
